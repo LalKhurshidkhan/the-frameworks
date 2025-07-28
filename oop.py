e034b9eb1044e946e1cbab3d1c668201fe747715
Lal Khurshid Khan <hilalkhursidkhan@gmail.com>
2025-07-29 00:07:00 +0500

    checking after reinstalling

--- oop.py
+++ oop.py
@@ -1,10 +1,12 @@
+# 
+
 class person:
     name = "khan"
     age = 25
     occupation = "DS"
+
     def info():
         print(f"{self.name} is {self.age} years old and works as {self.occupation}")
 
 a = person()
-
 a.info()
